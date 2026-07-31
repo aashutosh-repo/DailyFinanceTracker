ed52b003059471188de55c9824007f10d612aa24
Aashutosh Kumar <aashutoshkumar6729@gmail.com>
2026-07-31 14:24:48 +0530

    added missed file

--- src/main/java/com/finance/tracker/chatbot/indexing/FinancialIndexingService.java
+++ src/main/java/com/finance/tracker/chatbot/indexing/FinancialIndexingService.java
@@ -1,12 +1,16 @@
 package com.finance.tracker.chatbot.indexing;
 
+import com.finance.tracker.events.ChangeType;
+
 import java.time.YearMonth;
 
 public interface FinancialIndexingService {
 
     void indexMonthlySummary(String userId, YearMonth month);
+    void indexBudgetStatus(String userId, YearMonth month);
     void reindexUser(String userId);
-    private void deleteExistingSummary(String userId, YearMonth month) {
-        // TODO
-    }
+    void indexIncomeSummery(String userId, YearMonth month);
+    void indexExpenseSummery(String userId, YearMonth month);
+    void indexByChangeType(String userId, YearMonth month, ChangeType changeType);
+
 }
